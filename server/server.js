02a00e32e6cb90e7f9c45eb8624f011085f44278
DYuHLin <damianver2000@gmail.com>
2024-06-28 15:04:50 +0200

    Created and conencted to my Mongo database.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const App = express();
+const app = express();
+mongoose.set('strictQuery', false);
+const mongoDB = 'mongodb+srv://dyhlin2000:damian1216@cluster0.dtezc2s.mongodb.net/social_application?retryWrites=true&w=majority&appName=Cluster0';
 
-const server = App.listen(3000, () => console.log('App is listening'));
+async function main(){
+    mongoose.connect(mongoDB);
+};
+
+main().catch((err) => console.log(err));
+
+app.use(cors());
+app.use(express.json({limit: '50mb', extended: true}));
+app.use(cookieParser());
+app.use(session({secret: "cats", resave: false, saveUninitialized: true}));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
+
+const server = app.listen(3000, () => console.log('App is listening'));
