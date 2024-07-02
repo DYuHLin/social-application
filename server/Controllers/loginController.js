e16953a6f0a08ef4d0fb4c9bfa8f0bc276c6d90a
DYuHLin <damianver2000@gmail.com>
2024-07-02 16:08:10 +0200

    I completed the controllers for logging into the application, I am going to make some more adjustments in future as the application progresses..

---
+++ server/Controllers/loginController.js
@@ -0,0 +1,103 @@
+const asyncHandler = require('express-async-handler');
+const {body, validationResult} = require('express-validator');
+const users = require('../Models/user');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+let refreshTokens = [];
+
+const getAccessToken = (user) => {
+    return jwt.sign({user}, 'secretkey', {expiresIn: '10h'});
+};
+
+const getRefreshToken = (user) => {
+    return jwt.sign({user}, 'refreshsecretkey');
+};
+
+exports.post_login = asyncHandler(async (req, res, next) => {
+    try{
+        const user = await users.findOne({username: req.body.username}).populate('profileImg').populate('friends.user').populate([
+            {
+                path: 'friends.user',
+                populate: [{path: 'profileImg'}]
+            }
+        ]).exec();
+
+        if(!user){
+            console.log("Incorrect user");
+            return res.json("username");
+        };
+
+        const match = await bcrypt.compare(req.body.password, user.password);
+        if(!match){
+            console.log("Incorrect Password");
+            return res.json("password");
+        };
+
+        const accessToken = getAccessToken(user);
+        const refreshToken = getRefreshToken(user);
+
+        refreshTokens.push(refreshToken);
+
+        await users.updateOne({user: req.body.user}, {$set: {
+            online: true
+        }});
+
+        return res.json({
+            accessToken: accessToken,
+            refreshToken: refreshToken
+        });
+    }catch(err){
+        console.log(err);
+    };
+});
+
+exports.refresh_token = asyncHandler(async (req, res, next) => {
+    //token from user
+    const refreshToken = req.body.token;
+    //send error if there is no token or invalid
+    if(!refreshToken) return res.status(403).json("You are not authenticated");
+    if(refreshTokens.includes(refreshToken)){
+        return res.status(403).json("refresh token is not valid");
+    };
+    jwt.verify(refreshToken, "refreshsecretkey", (err, user) => {
+        err && console.log(err);
+        refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+
+        const newAccessToken = getAccessToken(user);
+        const newRefreshToken = getRefreshToken(user);
+
+        refreshTokens.push(newRefreshToken);
+
+        res.status(200).json({
+            accessToken: newAccessToken,
+            refreshToken: newRefreshToken
+        });
+    });
+});
+
+exports.post_logout = asyncHandler(async (req, res, next) => {
+    const refreshToken = req.body.token;
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+    await users.updateOne({username: req.body.username}, {$set: {
+        online: false
+    }});
+    res.status(200).json("You have logged out.");
+});
+
+exports.verifyToken = (req, res, next) =>{
+    const bearerHeader = req.headers['authorization'];
+    //check if bearer is undefined
+    if(typeof bearerHeader !== "undefined"){
+        //split the space in token
+        const bearer = bearerHeader.split(" ");
+        //get token from array
+        const bearerToken = bearer[1];
+        //set token
+        req.token = bearerToken;
+        //next middleware
+        next();
+    } else {
+        res.sendStatus(403);
+    };
+};
--- server/Models/comment.js
+++ server/Models/comment.js
@@ -6,6 +6,10 @@ const commentSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: "Users"},
     reply: {type: Schema.Types.ObjectId, ref: "Posts"},
     text: {type: String, required: true},
+    link: {type: String, required: true},
+    video: {type: String, required: true},
+    date: {type: Date, required: true, default: Date.now},
+    pics: [],
     likes: [
         {user: {type: Schema.Types.ObjectId, ref: "Users"}}
     ],
