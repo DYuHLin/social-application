85a911fdb5bfd00fe3abfa81ce48f07fec20aa30
DYuHLin <damianver2000@gmail.com>
2024-07-09 15:28:59 +0200

    Was able to add image uploading with Cloudinary.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import UploadImage from './components/UploadImage'
 
 function Register() {
+  const [name, setName] = useState('')
+  const [surname, setSurname] = useState('')
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirm, setConfirm] = useState('')
+  const [error, setError] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const updatedName = name.replace(/\s/g, '')
+    const updatedSurname = surname.replace(/\s/g, '')
+    const updatedUserame = username.replace(/\s/g, '')
+
+    const register = {name: updatedName, surname: updatedSurname, username: updatedUserame, email, password, confirmedPassword: confirm, image: ''}
+    try{
+      axios.post(`localhost:5000/auth/register`, register, {headers: {'Content-Type': 'application/json'}})
+      .then(res => res.data)
+      .then(status => {
+        if(status === 'failed'){
+          setError('This username has been taken')
+        } else if(status == 'match'){
+          setError('Your passwords do not match')
+        }else if(status == 'ok'){
+
+        }
+      })
+    }catch(err){
+      console.log(err)
+    }
+  }
+
   return (
     <section>
     <h1>Register</h1>
-        <form className='register-form' method="POST">
-            <input type="text" required name='name' id='name' className='name' placeholder='Name'/>
-            <input type="text" required name='surname' id='surname' className='surname' placeholder='Surname'/>
-            <input type="text" required name='username' id='username' className='username' placeholder='Username'/>
-            <input type="email" required name='email' id='email' className='email' placeholder='Email'/>
-            <input type="password" required name='password' id='password' className='password' placeholder='Password' minLength={6}/>
-            <input type="password" required name='confirmedPassword' id='confirmedPassword' className='confirmedPassword' placeholder='Confirm password' minLength={6}/>
-            <button>Register</button>
-            
+        <form className='register-form' method="POST" onSubmit={handleSubmit}>
+            <input type="text" required name='name' id='name' className='name' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
+            <input type="text" required name='surname' id='surname' className='surname' placeholder='Surname' value={surname} onChange={(e) => setSurname(e.target.value)}/>
+            <input type="text" required name='username' id='username' className='username' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <input type="email" required name='email' id='email' className='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <UploadImage />
+            <input type="password" required name='password' id='password' className='password' placeholder='Password' minLength={6} value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <input type="password" required name='confirmedPassword' id='confirmedPassword' className='confirmedPassword' placeholder='Confirm password' value={confirm} onChange={(e) => setConfirm(e.target.value)} minLength={6}/>
+            <button>Register</button>         
         </form>
         {/* <p className="error">{error}</p> */}
         <Link to={"/login"} className="link">Login</Link>
---
+++ client/src/pages/components/UploadImage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import axios from 'axios'
+
+function UploadImage() {
+
+    const UploadImage = (files) => {
+      //jag6ma0t
+        console.log(files[0])
+        const formData = new FormData()
+        formData.append('file', files[0])
+        formData.append('upload_preset', 'jag6ma0t')
+        
+        axios.post('https://api.cloudinary.com/v1_1/dqdoxrm2x/image/upload', formData).then((res) => {
+          console.log(res)
+        })
+    }
+
+  return (
+    <>
+    <input type="file" lable="Image" name="myFile" id="file-upload" accept='.jpeg, .png, .jpg' onChange={(e) => {UploadImage(e.target.files)}}/>      
+    </>
+  )
+}
+
+export default UploadImage
